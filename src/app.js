18bb2cadc4766c57bb7c00746e247d06f89bb484
Jplayskeys <joshuaadeniyi2000@gmail.com>
2022-06-19 00:31:14 +0100

    Built the TodoList with vanilla JS

---
+++ src/app.js
@@ -0,0 +1,151 @@
+import { ui } from './ui';
+
+//////------------Event Listeners
+const eventListeners = () => {
+    document
+        .querySelector('.btn-submit')
+        .addEventListener('click', taskSubmitted);
+    document
+        .querySelector('.todo__tasks')
+        .addEventListener('click', itemDeleteSubmit);
+    document
+        .querySelector('.todo__tasks')
+        .addEventListener('click', itemChecked);
+    document
+        .querySelector('.todo__tasks')
+        .addEventListener('click', itemUnchecked);
+    document
+        .querySelector('.todo__actions--1')
+        .addEventListener('click', itemAll);
+    document
+        .querySelector('.todo__actions--2')
+        .addEventListener('click', itemActive);
+    document
+        .querySelector('.todo__actions--3')
+        .addEventListener('click', itemCompleted);
+    // document
+    //     .querySelector('.todo__actions--4')
+    //     .addEventListener('click', item);
+};
+
+const taskSubmitted = (e) => {
+    e.preventDefault();
+    const taskTodo = document.querySelector('#inputtodo').value;
+    const taskDesc = document.querySelector('#inputdesc').value;
+    if (taskDesc && taskTodo) {
+        ui.addItems(taskTodo, taskDesc);
+        uiClear();
+    } else {
+        alert('Both fields cannot be empty');
+    }
+};
+const uiClear = () => {
+    document.querySelector('#inputtodo').value = '';
+    document.querySelector('#inputdesc').value = '';
+};
+const itemDeleteSubmit = (e) => {
+    if (
+        e.target.parentElement.classList.contains('todo__action--delete')
+    ) {
+        // console.log('didididi');
+        const idArr = e.target.parentElement.parentElement.id.split('-');
+        const idtarget = parseInt(idArr[1]);
+        ui.deleteItem(idtarget);
+        e.target.parentElement.parentElement.remove();
+    }
+    // console.log(document.querySelector('.todo__tasks')[selectedIndex]);
+};
+const itemChecked = (e) => {
+    // console.log(e.target.parentElement.nextElementSibling);
+    if (
+        e.target.parentElement.classList.contains('todo__action--check')
+    ) {
+        ui.selectedItem(e.target.parentElement.parentElement.id);
+        e.target.parentElement.nextElementSibling.style.zIndex = '1';
+        e.target.parentElement.nextElementSibling.style.opacity = '1';
+        e.target.parentElement.nextElementSibling.nextElementSibling.classList.add(
+            'todo__strikethru'
+        );
+    }
+    // if (e.target.parentElement.classList.contains('todo__strikethru')) {
+    //     // console.log('Great');
+    //     ui.unSelectedItem(e.target.parentElement.parentElement.id);
+    // }
+    return e.target.parentElement;
+};
+const itemUnchecked = (e) => {
+    if (
+        e.target.parentElement.parentElement.classList.contains(
+            'todo__completed'
+        )
+    ) {
+        e.target.parentElement.parentElement.nextElementSibling.classList.remove(
+            'todo__strikethru'
+        );
+
+        console.log(e.target.parentElement.parentElement.parentElement);
+        e.target.parentElement.parentElement.style.zIndex = '-2';
+        e.target.parentElement.parentElement.style.opacity = '0';
+        ui.unSelectedItem(
+            e.target.parentElement.parentElement.parentElement.id
+        );
+
+        // console.log(
+        //     e.target.parentElement.parentElement.previousElementSibling
+        // );
+    }
+};
+const itemAll = (e) => {
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains('todo__action')) {
+        // console.log('Good God');
+        e.target.parentElement.classList.remove('todo__action');
+    } else if (!e.target.parentElement.classList.contains('todo__action')) {
+        e.target.parentElement.classList.add('todo__action');
+        repeatedDOM();
+        e.target.parentElement.classList.add('todo__action');
+        ui.populateItemList(ui.dataStructure.listedTasks);
+    }
+    // console.log(e.target.parentElement);
+};
+const itemActive = (e) => {
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains('todo__action')) {
+        // console.log('Good God');
+        e.target.parentElement.classList.remove('todo__action');
+    } else if (!e.target.parentElement.classList.contains('todo__action')) {
+        e.target.parentElement.classList.add('todo__action');
+        repeatedDOM();
+        e.target.parentElement.classList.add('todo__action');
+    }
+};
+const itemCompleted = (e) => {
+    e.preventDefault();
+    let liDOMS = document.getElementsByClassName('todo__task');
+
+    liDOMS = Array.from(liDOMS);
+    let activeRender = ui.dataStructure.activeArr;
+
+    // ui.selectedItem(liDOMS, e.target.parentElement);
+
+    // console.log(liDOMS);
+    if (e.target.parentElement.classList.contains('todo__action')) {
+        // console.log('Good God');
+        e.target.parentElement.classList.remove('todo__action');
+    } else if (!e.target.parentElement.classList.contains('todo__action')) {
+        e.target.parentElement.classList.add('todo__action');
+        repeatedDOM();
+        e.target.parentElement.classList.add('todo__action');
+        ui.populateItemList(activeRender);
+    }
+};
+const repeatedDOM = () => {
+    let activeState = document.querySelectorAll('.todo__action');
+    activeState = Array.from(activeState);
+    activeState.forEach((eachItem) => {
+        // console.log(eachItem);
+        eachItem.classList.remove('todo__action');
+    });
+};
+ui.getTasks();
+eventListeners();
---
+++ src/ui.js
@@ -0,0 +1,225 @@
+class UI {
+    constructor(id, name, taskdesc) {
+        this.id = id;
+        this.name = name;
+        this.desc = taskdesc;
+    }
+    dataStructure = {
+        // return {
+        listedTasks: [
+            { id: 0, name: "I want to wash the plate, it's dirty" },
+            {
+                id: 1,
+                name: 'I need to do my assignments before it gets much',
+            },
+            {
+                id: 2,
+                name: 'I as  well need to  read and practice a lot',
+            },
+        ],
+        currentItem: false,
+        activeArr: [],
+        notActiveArr: [],
+        // };
+    };
+    //--------Fetch items from data structure
+    getTasks() {
+        const taskArr = this.dataStructure.listedTasks;
+        this.populateItemList(taskArr);
+    }
+    populateItemList(taskArr) {
+        let html = '';
+        taskArr.forEach((task) => {
+            html += ` <li class="todo__task" id="item-${task.id}">
+            <div class="todo__action--check" data-id="1">
+                <svg width="19" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m6.375 7.083 2.57 1.927c.148.112.358.09.48-.05l4.742-5.418"
+        stroke="#001F25"
+        stroke-width=".708"
+        stroke-linecap="round"
+      />
+      <path
+        d="M14.875 8.5a6.375 6.375 0 1 1-4.725-6.158"
+        stroke="#001F25"
+        stroke-width=".708"
+        stroke-linecap="round"
+      />
+    </svg>
+            </div>
+            <div class="todo__completed">
+                <svg width="19" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle
+        cx="9.5"
+        cy="9.5"
+        r="7.125"
+        fill="#001F25"
+      />
+      <path
+        d="m7.125 7.917 2.903 2.177c.149.111.358.09.48-.05l5.325-6.086"
+        stroke="#fff"
+        stroke-width=".316"
+        stroke-linecap="round"
+      />
+      <path
+        d="M16.625 9.5a7.125 7.125 0 1 1-5.281-6.882"
+        stroke="#fff"
+        stroke-width=".316"
+        stroke-linecap="round"
+      />
+    </svg>
+            </div>
+
+            <div class="todo__text">
+                ${task.name}
+            </div>
+            <div class="todo__action--delete">
+                <svg width="17" height="17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.083 3.719v.177h2.834v-.177a1.417 1.417 0 1 0-2.834 0Zm-.885.177v-.177a2.302 2.302 0 1 1 4.604 0v.177h3.985a.443.443 0 1 1 0 .885h-1.034l-.708 8.45a2.568 2.568 0 0 1-2.559 2.352H6.514a2.567 2.567 0 0 1-2.559-2.353l-.708-8.449H2.214a.443.443 0 0 1 0-.885h3.984Zm-1.36 9.26a1.682 1.682 0 0 0 1.676 1.542h3.972a1.682 1.682 0 0 0 1.677-1.542l.702-8.375h-8.73l.703 8.375Zm2.777-5.984a.443.443 0 1 0-.886 0v5.135a.443.443 0 0 0 .886 0V7.172Zm2.213-.443c.245 0 .443.199.443.443v5.135a.443.443 0 1 1-.886 0V7.172c0-.244.199-.443.443-.443Z"
+        fill="#001F25"
+      />
+    </svg>
+            </div>
+        </li>`;
+        });
+        //insert list items
+        document.querySelector('.todo__tasks').innerHTML = html;
+    }
+    addItems(taskTodo, taskDesc) {
+        let ID;
+        // Create ID
+        if (this.dataStructure.listedTasks.length > 0) {
+            ID =
+                this.dataStructure.listedTasks[
+                    this.dataStructure.listedTasks.length - 1
+                ].id + 1;
+        } else {
+            ID = 0;
+        }
+
+        // Create new item
+        let newItem = new UI(ID, taskTodo, taskDesc);
+        console.log(newItem);
+        console.log(this.dataStructure.listedTasks);
+
+        // Add to items array
+        this.dataStructure.listedTasks.push(newItem);
+
+        this.addListItem(newItem);
+    }
+    addListItem(task) {
+        const li = document.createElement('li');
+        li.className = 'todo__task';
+        li.id = `item-${task.id}`;
+        li.innerHTML = `
+        <div class="todo__action--check" data-id="1">
+            <svg width="19" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="m6.375 7.083 2.57 1.927c.148.112.358.09.48-.05l4.742-5.418"
+    stroke="#001F25"
+    stroke-width=".708"
+    stroke-linecap="round"
+  />
+  <path
+    d="M14.875 8.5a6.375 6.375 0 1 1-4.725-6.158"
+    stroke="#001F25"
+    stroke-width=".708"
+    stroke-linecap="round"
+  />
+</svg>
+        </div>
+        <div class="todo__completed">
+            <svg width="19" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle
+    cx="9.5"
+    cy="9.5"
+    r="7.125"
+    fill="#001F25"
+  />
+  <path
+    d="m7.125 7.917 2.903 2.177c.149.111.358.09.48-.05l5.325-6.086"
+    stroke="#fff"
+    stroke-width=".316"
+    stroke-linecap="round"
+  />
+  <path
+    d="M16.625 9.5a7.125 7.125 0 1 1-5.281-6.882"
+    stroke="#fff"
+    stroke-width=".316"
+    stroke-linecap="round"
+  />
+</svg>
+        </div>
+
+        <div class="todo__text">
+            ${this.shortenText(task.name, 40)}
+        </div>
+        <div class="todo__action--delete">
+            <svg width="17" height="17" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M7.083 3.719v.177h2.834v-.177a1.417 1.417 0 1 0-2.834 0Zm-.885.177v-.177a2.302 2.302 0 1 1 4.604 0v.177h3.985a.443.443 0 1 1 0 .885h-1.034l-.708 8.45a2.568 2.568 0 0 1-2.559 2.352H6.514a2.567 2.567 0 0 1-2.559-2.353l-.708-8.449H2.214a.443.443 0 0 1 0-.885h3.984Zm-1.36 9.26a1.682 1.682 0 0 0 1.676 1.542h3.972a1.682 1.682 0 0 0 1.677-1.542l.702-8.375h-8.73l.703 8.375Zm2.777-5.984a.443.443 0 1 0-.886 0v5.135a.443.443 0 0 0 .886 0V7.172Zm2.213-.443c.245 0 .443.199.443.443v5.135a.443.443 0 1 1-.886 0V7.172c0-.244.199-.443.443-.443Z"
+    fill="#001F25"
+  />
+</svg>
+        </div>
+    `;
+        document
+            .querySelector('.todo__tasks')
+            .insertAdjacentElement('beforeend', li);
+    }
+    getCurrentItem() {
+        return this.dataStructure.currentItem;
+    }
+    deleteItem(id) {
+        const ids = this.dataStructure.listedTasks.map((item) => {
+            return item.id;
+        });
+        const index = ids.indexOf(id);
+        this.dataStructure.listedTasks.splice(index, 1);
+        // console.log(this.dataStructure.listedTasks);
+    }
+    shortenText(text, max) {
+        return text && text.length > max ?
+            text.slice(0, max).split(' ').slice(0, -1).join(' ') + '...' :
+            text;
+    }
+    selectedItem(val) {
+        let id = parseInt(val.split('-')[1]);
+
+        const ids = this.dataStructure.listedTasks.map((item) => {
+            return item.id;
+        });
+
+        // let activeArr = [];
+        this.dataStructure.listedTasks.map((eachItem) => {
+            if (id === eachItem.id) {
+                this.dataStructure.activeArr.push(eachItem);
+                const index = ids.indexOf(id);
+                console.log(this.dataStructure.activeArr);
+            }
+        });
+        // console.log(ids);
+        if (val) {
+            this.dataStructure.currentItem = true;
+            // console.log(this.dataStructure);
+        }
+    }
+    unSelectedItem(val) {
+        let id = parseInt(val.split('-')[1]);
+
+        const ids = this.dataStructure.listedTasks.map((item) => {
+            return item.id;
+        });
+
+        // let activeArr = [];
+        this.dataStructure.listedTasks.map((eachItem) => {
+            if (id === eachItem.id) {
+                const index = ids.indexOf(id);
+                this.dataStructure.activeArr.splice(index, 1);
+                console.log(this.dataStructure.activeArr);
+            }
+        });
+    }
+}
+export const ui = new UI();
